16e5d5de49a0d810a731399afef810806566b6c9
Spectrum Dev <spectrumaurorawar@gmail.com>
2026-09-22 21:12:55 +0300

    feat: add initial main.c entry point

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("Arch Linux System Information Fetcher\n");
+    printf("-------------------------------------\n");
+    return EXIT_SUCCESS;
+}
